fa29162985529b50413982d516bbf824b4b2f59f
Efi <brunofuchs2002@gmail.com>
2025-05-14 15:02:20 -0300

    likwid 2

--- script.sh
+++ script.sh
@@ -2,9 +2,18 @@
 # Global Config
 LIKWID_HOME=/home/soft/likwid
 
+# Clean Up
+make purge > /dev/null
 
 # Compile
 make > /dev/null
 
 # Freq Fixed
 echo "performance" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
+
+# Call program
+likwid-perfctr --output flops_dp.txt -C 0 -g FLOPS_DP -m ./resolveEDO < edo.dat
+
+# Final
+# Restore the original value
+echo "powersave" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
